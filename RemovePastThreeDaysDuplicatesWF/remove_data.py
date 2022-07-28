6c641c278b0a65232b721a0c7cc3eaa58f9c2e47
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2022-07-28 14:13:32 +0530

    Added last three days removal on duplicates

--- RemovePastThreeDaysDuplicatesWF/remove_data.py
+++ RemovePastThreeDaysDuplicatesWF/remove_data.py
@@ -25,27 +25,39 @@ curr_date   : finds todays date
 
 import pandas as pd
 from datetime import date, datetime, timedelta
-
+# from Database.db import *
 input_dir = "E:\\luigi\\ReportMergeWF\\Output\\"
+# input_dir = "E:\\luigi\\DailyRunWF\\Output\\"
 output_dir = "E:\\luigi\\RemovePastThreeDaysDuplicatesWF\\Output\\"
 #
-input_file_template = "PREIPO_Final_Report_{date}.csv"
-output_file_template = "PREIPO_Final_Report_{date}_2.csv"
+
 #
-no_of_days = 3
-curr_date = date.today() -timedelta(days=1)
 
 
+def check_title(df,title):
+    try:
+        return df["title"].to_list().index(title)
+    except:
+        return -1
+def check_text(df,text):
+    try:
+        return df["text"].to_list().index(text)
+    except:
+        return -1
 # remove duplicate Function
 def remove_duplicates_from_todays_file(input_dir, output_dir):
     # Create File input and output names
+    input_file_template = "PREIPO_Final_Report_{date}.csv"
+    output_file_template = "PREIPO_Final_Report_{date}_2.csv"
     input_file = input_dir + input_file_template
     output_file = output_dir + output_file_template
+    no_of_days = 3
+    curr_date = date.today()
     # To store dataframes including todays and find todays date
     df_list = []
-    #
     # Adds todays data and then prev data
-    for i in range(1,no_of_days + 1):
+    df1 = pd.read_csv(input_dir + input_file_template.format(date=str(curr_date.strftime("%Y-%m-%d"))))
+    for i in range(1,no_of_days+1):
 
         # Reading csv file and adding df to df_list
         file = input_file.format(date=(curr_date - timedelta(days=i)))
@@ -54,13 +66,17 @@ def remove_duplicates_from_todays_file(input_dir, output_dir):
 
     # Concatenating dataframes in df_list
     df_concat = pd.concat(df_list)
-    #
+    collection = []
+    for i,row in enumerate(df1.values):
+        if(check_text(df_concat,df1["text"][i]) == -1 and check_title(df_concat,df1["title"][i]) == -1):
+            collection.append(row.tolist())
+    new_df = pd.DataFrame(collection,columns=["Unnamed: 0","publish_date","scraped_date","title","text","Companies","Country","link","Comments","update"])
     # Droping Duplicates by keeping today data and resetting index
-    new_df = df_concat.drop_duplicates(subset=["text", "title"]).reset_index(drop=True)
+    new_df = new_df.drop_duplicates(subset=["text", "title"]).reset_index(drop=True)
     
     # Writing data to output_file
     new_df.to_csv(output_file.format(date=curr_date),index=False)
-
+    
 
 # For testing this file run this file with terminal in the RemovePastThreeDaysDuplicate folder
 if __name__ == "__main__":
---
+++ RemovePastThreeDaysDuplicatesWF/removepastthreedays_workflow.py
@@ -0,0 +1,14 @@
+from os import remove
+import luigi 
+from RemovePastThreeDaysDuplicatesWF.remove_data import *
+from datetime import date
+class removeDataWorkflow(luigi.Task):
+    input_dir = luigi.Parameter(default="E:\\luigi\\ReportMergeWF\\Output\\")
+    # input_dir = "E:\\luigi\\DailyRunWF\\Output\\"
+    output_dir = luigi.Parameter(default="E:\\luigi\\RemovePastThreeDaysDuplicatesWF\\Output\\")
+    dat =str(date.today().strftime("%Y-%m-%d"))
+    def requires(self):
+        return luigi.LocalTarget(self.output_dir + "PREIPO_Final_Report_" + self.dat + ".csv")
+    def run(self):
+        remove_duplicates_from_todays_file(self.input_dir,self.output_dir)
+    
