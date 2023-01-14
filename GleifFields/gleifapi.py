b5772411b92d79eea8474a786122e57314af10d5
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2023-01-14 13:45:50 +0530

    Added country and company search for key fields

--- GleifFields/gleifapi.py
+++ GleifFields/gleifapi.py
@@ -1,6 +1,9 @@
 import requests
 import json
 import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+from pygleif import PyGleif
 def company_info(companyname):
         #api_url = "https://api.gleif.org/api/v1/fuzzycompletions?field=entity.legalName&q=ABB E-mobility"
         api_url="https://api.gleif.org/api/v1/fuzzycompletions?field=entity.legalName&q="+companyname
@@ -42,46 +45,66 @@ def company_info(companyname):
                         print("headquarterAddress : ",headquarterAddress)
 
 
-def company_info1(companyname):
+def company_info1(companyname,iso):
         api_url="https://api.gleif.org/api/v1/fuzzycompletions?field=entity.legalName&q="+str(companyname)
         response = requests.get(api_url)
         json_format = json.loads(response.text)
-        print(json_format["data"])
         res_obj = {"company_name":None,"legalAddress":None,"officeAddress":None,"country":None,"LEI":None}
+        objects = []
         try:
-                i = json_format['data'][0]
+                for i in json_format['data']:
+        
+                        key='relationships'
+                        x = list(i.keys())
+                        if(x.count(key) == 1):
+                                api_url1=i['relationships']['lei-records']['links']['related']
+                                response1=requests.get(api_url1)
+                                json_format1 = json.loads(response1.text)
+                                res_obj["company_name"] = json_format1['data']['attributes']['entity']['legalName']['name']
+                                legalAddress_details=""
+                                for j in json_format1['data']['attributes']['entity']['legalAddress']['addressLines']:
+                                        legalAddress_details=str(legalAddress_details) + str(j)
+                                legalAddress=str(legalAddress_details)+" "+str(json_format1['data']['attributes']['entity']['legalAddress']['city'])+ " "+str(json_format1['data']['attributes']['entity']['legalAddress']['region'])+ " "+str(json_format1['data']['attributes']['entity']['legalAddress']['country'])
+                                res_obj["legalAddress"] = legalAddress
+                                res_obj["country"] = json_format1['data']['attributes']['entity']['legalAddress']['country']
+                                HQAddress_details = ""
+                                for j in json_format1['data']['attributes']['entity']['headquartersAddress']['addressLines']:
+                                        HQAddress_details=str(HQAddress_details) + str(j)
+                                
+                                headquarterAddress = str(HQAddress_details) + " " + str(json_format1['data']['attributes']['entity']['headquartersAddress']['city']) + " "+ str(json_format1['data']['attributes']['entity']['headquartersAddress']['region'])+ " "+str(json_format1['data']['attributes']['entity']['headquartersAddress']['country'])
+                                res_obj["officeAddress"] = headquarterAddress
+                                res_obj["LEI"] = json_format['data'][0]["relationships"]["lei-records"]["data"]["id"]
+                                if iso in headquarterAddress.strip().split():
+                                        objects.append(res_obj)
         except:
                 return res_obj
-        key='relationships'
-        x = list(i.keys())
-        if(x.count(key) == 1):
-                api_url1=i['relationships']['lei-records']['links']['related']
-                response1=requests.get(api_url1)
-                json_format1 = json.loads(response1.text)
-                res_obj["company_name"] = json_format1['data']['attributes']['entity']['legalName']['name']
-                legalAddress_details=""
-                for j in json_format1['data']['attributes']['entity']['legalAddress']['addressLines']:
-                        legalAddress_details=str(legalAddress_details) + str(j)
-                legalAddress=str(legalAddress_details)+" "+str(json_format1['data']['attributes']['entity']['legalAddress']['city'])+ " "+str(json_format1['data']['attributes']['entity']['legalAddress']['region'])+ " "+str(json_format1['data']['attributes']['entity']['legalAddress']['country'])
-                res_obj["legalAddress"] = legalAddress
-                res_obj["country"] = json_format1['data']['attributes']['entity']['legalAddress']['country']
-                HQAddress_details=""
-                for j in json_format1['data']['attributes']['entity']['headquartersAddress']['addressLines']:
-                        HQAddress_details=str(HQAddress_details) + str(j)
-                
-                headquarterAddress=str(HQAddress_details)+" "+str(json_format1['data']['attributes']['entity']['headquartersAddress']['city'])+ " "+str(json_format1['data']['attributes']['entity']['headquartersAddress']['region'])+ " "+str(json_format1['data']['attributes']['entity']['headquartersAddress']['country'])
-                res_obj["officeAddress"] = headquarterAddress
-                res_obj["LEI"] = json_format['data'][0]["relationships"]["lei-records"]["data"]["id"]
-        return res_obj
-
+        if len(objects) == 0:
+                return {"company_name":None,"legalAddress":None,"officeAddress":None,"country":None,"LEI":None}
+        return objects[0] 
+def get_jurisdiction_info(lei):
+        try:
+                gleif = PyGleif(lei)
+                return gleif.response.data.attributes.entity.jurisdiction
+        except:
+                return ""
+        
 def generate_final_file(df):
         la,oa,lei = [],[],[]
         for i,row in df.iterrows():
-                data = company_info1(row["Companies"])
-                print(data)
-                la.append(data["legalAddress"])
-                oa.append(data["officeAddress"])
-                lei.append(data["LEI"])
+                company = row["Companies"]
+                if "," in row["Companies"]:
+                        company = row["Companies"].strip().split(",")[0]
+                print(company)
+                # word_length = len(company.split())
+                data = company_info1(" ".join(company.strip().split()[:-1]),row["Country"].strip())
+                if row["Country"] == data["country"]:
+                        la.append(data["legalAddress"])
+                        oa.append(data["officeAddress"])
+                        lei.append(data["LEI"])
+                else:
+                        la.append("")
+                        oa.append("")
+                        lei.append("")
         df["LegalAddress"] = la
         df["OfficeAddress"] = oa
         df["LEI"] = lei
@@ -91,5 +114,8 @@ def generate_final_file(df):
 
 #company_info("ABB E-mobility")
 if __name__ == "__main__":
-        df = pd.read_excel("PREIPO_2022-12-20.xlsx")
+        df = pd.read_excel("PREIPO_2023-01-02.xlsx")
         print(generate_final_file(df))
+        # get_company_info("DIVGI TORQTRANSFER SYSTEMS LIMITED","IN")
+        # "335800N9OHIPOMBP7C30"
+        # print(company_info1("Dar Al Arkan","UAE"))
