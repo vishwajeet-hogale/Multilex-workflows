ec36293acd44a2bfeb6e50991b779c8573157304
Sai Manasa <191381679manasaab@gmail.com>
2023-02-17 11:37:52 +0530

    Added uzdaily()

--- DailyRunWF/Pipeline/Scraper/scraper_asynch.py
+++ DailyRunWF/Pipeline/Scraper/scraper_asynch.py
@@ -7892,6 +7892,164 @@ def multilex_scraper(input_dir, output_dir):
             not_working_functions.append("dw")
             print("dw not working")
 
+    def uzdaily(keyword):
+        try:
+            print("uzdaily")
+            Errors["uzdaily"]=[]
+            
+            url = f"https://www.uzdaily.uz/en/search?q={keyword}"
+            domain_url = "https://www.uzdaily.uz/en"
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0",
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                'sec-fetch-site': 'none',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-user': '?1',
+                'sec-fetch-dest': 'document',
+                'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            }
+            div_class="dis_flex box_shadow margin_block"# Class name of div containing the a tag
+            #h1_class = "_1Y-96"
+            #h1_div_class = "col-xs-12"
+            title_h3_class= "header"
+            date_p_class= ["date"]
+            para_div_class=["main links"]
+
+            links=[]
+            try:
+                page = requests.get(url, headers=headers)
+                soup = BeautifulSoup(page.content, "html.parser")
+            except:
+                print("uzdaily not working")
+                not_working_functions.append('uzdaily')
+                err = "Main link did not load: " + url
+                Errors["uzdaily"].append(err)
+                return
+            
+            
+            try:
+                
+                for divtag in soup.find_all("div", {"class": div_class}):
+                    for a in divtag.find_all("a", href=True):
+                        link = a["href"]  # Gets the link
+
+                    
+
+                    # Filtering advertaisment links
+                        link_start = domain_url
+                        if link.startswith(link_start):
+                            links.append(link)
+            except:
+                if len(links)==0:
+                    print("uzdaily not working")
+                    not_working_functions.append('uzdaily')
+                    Errors["uzdaily"].append("Extraction of link not working.")
+                    return
+                        
+            # Remove duplicates
+            links = list(set(links))
+            
+            # links # Debugging - if link array is generated
+            collection = []
+            scrapper_name = "uzdaily"
+            
+            def getarticles(link):
+                flag=0
+                err=err_dict()
+                try:
+                    l_page = requests.get(link, headers=headers)
+                    l_soup = BeautifulSoup(l_page.content, 'html.parser')
+                except:
+                    err["link"]="Link not working: "+link
+                    Errors["uzdaily"].append(err)
+                    return
+                
+                data = []
+                
+                # Scraping the heading
+                #h1_ele = l_soup.find("h1", {"class": h1_class})
+                
+                try:
+                    title_ele = l_soup.find("h3")
+                    data.append(title_ele.text)
+                except:
+                    err["link"]=link
+                    err['title']="Error"
+                    data.append("-")
+                    flag=1
+                 # drops the complete data if there is an error
+                # Adding the link to data
+                data.append(link)
+                # Scraping the published date
+                try:
+                    date_ele = l_soup.find("p", {"class": date_p_class})
+                    date_text = date_ele.text
+                    l=date_text.split(" ")
+                    date_text=l[0]
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    #date_text = (date_text.split('/'))[-1]
+                    #date_text = date_text.replace(" Updated: ", "")
+                    # The date_text could be further modified to represent a proper date format
+                    data.append(date_text)
+                except:
+                    err["link"]=link
+                    err['published_date']="Error"
+                    data.append("-")
+                    flag=1
+              # drops the complete data if there is an error
+                # Adding the scraped date to data
+                today = date.today()
+                cur_date = str(today)
+                data.append(cur_date)
+                # Scraping the paragraph
+                try:
+                    para_ele = (l_soup.findAll("div", {"class": para_div_class}))[-1]
+                    data.append(para_ele.text)  # Need to make this better
+                except:
+                    err["link"]=link
+                    err['text']="Error"
+                    data.append("-")
+                    flag=1
+                  # drops the complete data if there is an error
+                # Adding data to a collection
+                
+                if flag==1:
+                    Errors["uzdaily"].append(err)
+                
+                collection.append(data)
+                
+            thread_list=[]
+            length=len(links)
+            for i in range(length):
+                thread_list.append(threading.Thread(target=getarticles, args=(links[i], )))
+            
+            for thread in thread_list:
+                thread.start()
+            
+            for thread in thread_list:
+                thread.join()
+            
+            df = pd.DataFrame(collection, columns=[
+                              'title', 'link', 'publish_date', 'scraped_date', 'text'])
+            
+            
+            # print(df) # For debugging. To check if df is created
+            # print(err_logs) # For debugging - to check if any errors occoured
+            df = FilterFunction(df)
+            emptydataframe("uzdaily", df)
+            # df  = link_correction(df)
+            return df
+        
+        except:
+            not_working_functions.append("uzdaily")
+            print("uzdaily not working")
+
 
             
     #                                  Final
@@ -7977,9 +8135,12 @@ def multilex_scraper(input_dir, output_dir):
     df78=dw("ipo")
     df79=dw("fpo")
     df80=dw("spac")
+    df81=uzdaily("ipo")
+    df82=uzdaily("fpo")
+    df83=uzdaily("spac")
 
 
-    df_final_1 = [ df1, df2, df3, df4, df5, df6, df7, df8, df9, df10 , df11, df12, df13, df14, df15, df16, df17, df18, df19, df20 , df21, df22, df23, df24, df25, df26, df27, df28, df29, df30, df31, df32, df33, df34, df35, df36, df37 ,df38, df39, df43, df44, df45, df46, df47, df48, df49, df50, df51, df52, df53, df54, df55, df56, df57, df58, df59, df60, df61, df62, df63, df64, df65, df66, df67, df68, df69, df70, df71, df72, df73, df74, df75, df76, df77, df78, df79, df80]
+    df_final_1 = [ df1, df2, df3, df4, df5, df6, df7, df8, df9, df10 , df11, df12, df13, df14, df15, df16, df17, df18, df19, df20 , df21, df22, df23, df24, df25, df26, df27, df28, df29, df30, df31, df32, df33, df34, df35, df36, df37 ,df38, df39, df43, df44, df45, df46, df47, df48, df49, df50, df51, df52, df53, df54, df55, df56, df57, df58, df59, df60, df61, df62, df63, df64, df65, df66, df67, df68, df69, df70, df71, df72, df73, df74, df75, df76, df77, df78, df79, df80, df81, df82, df83]
 
     
     
