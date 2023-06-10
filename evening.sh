253a7c08e41071181dbe5b17878384e16028f27e
Ujwal6702 <ujwalujwalc#gmail.com>
2023-06-11 01:40:05 +0530

    Test 2

--- evening.sh
+++ evening.sh
@@ -1,3 +1,4 @@
 cd /home/rishika/Multilex-workflows
-echo sharika1709$ | sudo -S python3 -m luigi --module evening Part1EveningPipeline --local-scheduler
+
+echo sharika1709$ | sudo -S python3 -m luigi --module evening Part2EveningPipeline_new --local-scheduler
 
