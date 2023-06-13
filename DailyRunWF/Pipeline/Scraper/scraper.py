c6a99fa27711c0bb36cb64fae2795eac66f5676a
Sai Manasa <191381679manasaab@gmail.com>
2023-06-13 13:11:15 +0530

    Added cryptopolitan()

--- DailyRunWF/Pipeline/Scraper/scraper.py
+++ DailyRunWF/Pipeline/Scraper/scraper.py
@@ -18166,6 +18166,158 @@ def multilex_scraper(input_dir, output_dir):
             not_working_functions.append("dailyexcelsior")
             print("dailyexcelsior not working")
 
+    def cryptopolitan(keyword):
+        try:
+            print("cryptopolitan")
+            Errors["cryptopolitan"]=[]
+
+
+            url = f"https://www.cryptopolitan.com/?s={keyword}"
+            domain_url = "https://www.cryptopolitan.com/"
+
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0",
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                'sec-fetch-site': 'none',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-user': '?1',
+                'sec-fetch-dest': 'document',
+                'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            }
+
+
+            links=[]
+            try:
+                page = requests.get(url, headers=headers)
+                soup = BeautifulSoup(page.content, "html.parser")
+            except:
+                print("cryptopolitan not working")
+                not_working_functions.append('cryptopolitan')
+                err = "Main link did not load: " + url
+                Errors["cryptopolitan"].append(err)
+                return
+
+
+            try:
+
+                all_divs = soup.find_all("a",{"class":"grid-image-holder"})
+                for div in all_divs:
+                    links.append(div["href"])
+
+            except:
+                if len(links)==0:
+                    print("cryptopolitan not working")
+                    not_working_functions.append('cryptopolitan')
+                    Errors["cryptopolitan"].append("Extraction of link not working.")
+                    return
+
+            # Remove duplicates
+            links = list(set(links))
+
+            # links # Debugging - if link array is generated
+            collection = []
+            scrapper_name = "cryptopolitan"
+
+            def getarticles(link):
+
+                flag=0
+                err=err_dict()
+                try:
+                    l_page = requests.get(link, headers=headers)
+                    l_soup = BeautifulSoup(l_page.content, 'html.parser')
+                except:
+                    err["link"]="Link not working: "+link
+                    Errors["cryptopolitan"].append(err)
+                    return
+
+                data = []
+
+                # Scraping the heading
+                #h1_ele = l_soup.find("h1", {"class": h1_class})
+
+                try:
+                    title_ele = l_soup.find("h1",{"class":"entry-title"})
+                    title_text = title_ele.text
+                    title_text = title_text. strip("\n ")
+                    data.append(title_text)
+                   
+                   
+                except:
+                    err["link"]=link
+                    err['title']="Error"
+                    data.append("-")
+                    flag=1
+                 # drops the complete data if there is an error
+                # Adding the link to data
+                data.append(link)
+                # Scraping the published date
+                try:
+
+                    date_ele = l_soup.find("div",{"class":"cp-entry-meta-left"})
+                    date_text = date_ele.text
+                    l=date_text.split(' ')
+                    date_text=l[4].strip(",")+"-"+l[3]+"-"+l[5]
+                    data.append(date_text)
+                except:
+                    err["link"]=link
+                    err['published_date']="Error"
+                    data.append("-")
+                    flag=1
+              # drops the complete data if there is an error
+                # Adding the scraped date to data
+                today = date.today()
+                cur_date = str(today)
+                data.append(cur_date)
+                # Scraping the paragraph
+
+                try:
+                    para_ele = (l_soup.findAll("div",{"class":"entry-content"}))[-1]
+                    para_text = para_ele.text
+                    para_text = para_text.strip("\n ")
+                    data.append(para_text)
+                   
+                    # Need to make this better
+                except:
+                    err["link"]=link
+                    err['text']="Error"
+                    data.append("-")
+                    flag=1
+                  # drops the complete data if there is an error
+                # Adding data to a collection
+
+                if flag==1:
+                    Errors["cryptopolitan"].append(err)
+
+                collection.append(data)
+
+
+            thread_list=[]
+            length=len(links)
+            for i in range(length):
+                thread_list.append(threading.Thread(target=getarticles, args=(links[i], )))
+
+            for thread in thread_list:
+                thread.start()
+
+            for thread in thread_list:
+                thread.join()
+
+            df = pd.DataFrame(collection, columns=[
+                              'title', 'link', 'publish_date', 'scraped_date', 'text'])
+
+
+            # print(df) # For debugging. To check if df is created
+            # print(err_logs) # For debugging - to check if any errors occoured
+            df = FilterFunction(df)
+            emptydataframe("cryptopolitan", df)
+            # df  = link_correction(df)
+
+            return df
+
+        except:
+            not_working_functions.append("cryptopolitan")
+            print("cryptopolitan not working")
+
 
     
     ################################################################################################
@@ -18500,10 +18652,11 @@ def multilex_scraper(input_dir, output_dir):
 
     df296 = madrastribune("ipo")
     df297 = dailyexcelsior("ipo")
+    df298=cryptopolitan("ipo")
 
 
 
-    df_final_1 = [df170, df1, df2, df3, df4, df5, df6, df7, df11, df12, df13, df14, df15, df16, df17, df18, df19, df20 , df21, df22, df23, df24, df25, df26, df27, df28, df29, df30, df31, df32, df33, df34, df35, df36, df37 , df43,  df46, df49, df52,  df55, df57,  df60,  df63,  df66,  df69,  df72,  df75,  df78,  df81, df140,df146, df152,  df155,  df158,  df161,  df164,  df167,  df173,  df176,  df179,  df182,  df185,  df40,  df188,  df191,  df194,  df197,  df200,  df203, df207,  df210,  df213,  df216,  df219, df222,df225,df228,df231,df234,df237,df240,df243,df246,df249,df252,df255,df257,df258,df260,df261,df263,df264,df257,df258,df260,df261,df263,df264,df267,df270,df273,df276,df277,df280,df281,df284,df287,df288,df289,df292,df293,df294,df295,df296,df297]
+    df_final_1 = [df170, df1, df2, df3, df4, df5, df6, df7, df11, df12, df13, df14, df15, df16, df17, df18, df19, df20 , df21, df22, df23, df24, df25, df26, df27, df28, df29, df30, df31, df32, df33, df34, df35, df36, df37 , df43,  df46, df49, df52,  df55, df57,  df60,  df63,  df66,  df69,  df72,  df75,  df78,  df81, df140,df146, df152,  df155,  df158,  df161,  df164,  df167,  df173,  df176,  df179,  df182,  df185,  df40,  df188,  df191,  df194,  df197,  df200,  df203, df207,  df210,  df213,  df216,  df219, df222,df225,df228,df231,df234,df237,df240,df243,df246,df249,df252,df255,df257,df258,df260,df261,df263,df264,df257,df258,df260,df261,df263,df264,df267,df270,df273,df276,df277,df280,df281,df284,df287,df288,df289,df292,df293,df294,df295,df296,df297,df298]
     
     
        
