4a1fbdb258028b46e591b20543eaa9008b6ec8c5
Ujwal6702 <ujwalujwalc#gmail.com>
2023-06-19 03:06:47 +0530

    Added arvopaperi

--- DailyRunWF/Pipeline/Scraper/scraper.py
+++ DailyRunWF/Pipeline/Scraper/scraper.py
@@ -18897,6 +18897,160 @@ def multilex_scraper(input_dir, output_dir):
         except:
             not_working_functions.append("dailysabah")
             print("dailysabah not working")
+    
+    
+    def arvopaperi(arvopaperi="IPO"):
+        try:
+            print("arvopaperi")
+            Errors['arvopaperi']=[]
+
+            url = f"https://www.arvopaperi.fi/listautumiset"
+            domain_url = "https://www.arvopaperi.fi"
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:78.0) Gecko/20100101 Firefox/78.0",
+                    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                    'sec-fetch-site': 'none',
+                    'sec-fetch-mode': 'navigate',
+                    'sec-fetch-user': '?1',
+                    'sec-fetch-dest': 'document',
+                    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            }
+            links=[]
+            try:
+                page = requests.get(url, headers=headers)
+                soup = BeautifulSoup(page.content, "html.parser")
+            except:
+                print("arvopaperi not working")
+                not_working_functions.append('arvopaperi')
+                err = "Main link did not load: " + url
+                Errors["arvopaperi"].append(err)
+                return
+            
+            try:
+                for i in soup.find("div", class_="sc-9j30fl-0 bjJNEh").find_all("a", class_="sc-pek81u-0 gaa-DNB sc-mzr2r-0 iMdguU"):
+                    link = i["href"]  # Gets the link
+                    link = domain_url + link
+                    links.append(link)
+
+            except:
+                if len(links)==0:
+                    print("arvopaperi not working")
+                
+                    not_working_functions.append('arvopaperi')
+                    Errors["arvopaperi"].append("Extraction of link not working.")
+                    return
+            
+            # Remove duplicates
+            links = list(set(links))
+            print(len(links))
+
+            # links # Debugging - if link array is generated
+            collection = []
+            scrapper_name = "arvopaperi"
+
+
+            def getarticles(link):
+                
+                flag=0
+                err=err_dict()
+                try:
+                    l_page = requests.get(link, headers=headers)
+                    l_soup = BeautifulSoup(l_page.content, 'html.parser')
+                except:
+                    err["link"]="Link not working: "+link
+                    Errors["arvopaperi"].append(err)
+                    return
+
+                data = []
+
+                # Scraping the heading
+                #h1_ele = l_soup.find("h1", {"class": h1_class})
+
+                try:
+                    title_ele = l_soup.find("h1", class_="sc-nwx7kx-2 fgVpie")
+                    title_text = title_ele.text
+                    title=translatedeep(title_text.strip())
+                    
+                except:
+                    err["link"]=link
+                    err['title']="Error"
+                    title = "-"
+                    flag=1
+                    # drops the complete data if there is an error
+                # Adding the link to data
+                
+
+                # Scraping the published date
+                try:
+
+                    date_ele = l_soup.find("span", {'class' : 'sc-1uuafvy-0 kSfUkP'}).time
+                    date_text = date_ele.get("datetime")
+                    l="-".join(date_text.split("T")[0].split("-")[::-1])
+                    
+                
+                except:
+                    err["link"]=link
+                    err['published_date']="Error"
+                    l="-"
+                    flag=1
+                # drops the complete data if there is an error
+                # Adding the scraped date to data
+                today = date.today()
+                cur_date = str(today)
+                
+
+                # Scraping the paragraph
+
+                try:
+                    para_ele = l_soup.find("p", {'class' : 'sc-wpntry-0 cHGlB'})
+                    para_text = para_ele.text
+                    para_text = para_text.strip("\n ")
+                    para = translatedeep(para_text)
+                    # Need to make this better
+                except:
+                    err["link"]=link
+                    err['text']="Error"
+                    para = "-"
+                    flag=1
+                    # drops the complete data if there is an error
+                # Adding data to a collection
+
+                if flag==1:
+                    Errors["arvopaperi"].append(err)
+                
+                data.append(title)
+                data.append(link)
+                data.append(l)
+                data.append(cur_date)
+                data.append(para)
+                
+                collection.append(data)
+
+            thread_list=[]
+            length=len(links)
+            for i in range(length):
+                thread_list.append(threading.Thread(target=getarticles, args=(links[i], )))
+
+            for thread in thread_list:
+                thread.start()
+
+            for thread in thread_list:
+                thread.join()
+
+            df = pd.DataFrame(collection, columns=[
+                                'title', 'link', 'publish_date', 'scraped_date', 'text'])
+            
+            # print(df) # For debugging. To check if df is created
+            # print(Errors) # For debugging - to check if any errors occoured
+            df = FilterFunction(df)
+            emptydataframe("arvopaperi", df)
+            # df  = link_correction(df)
+
+            return df
+
+        except:
+            not_working_functions.append("arvopaperi")
+            print("arvopaperi not working")
 
 
     
@@ -18907,6 +19061,7 @@ def multilex_scraper(input_dir, output_dir):
     #df150=bankok_post("fpo")
     #df151=bankok_post("spac")
     df55=stock_eastmoney()
+    df303=arvopaperi()
     
     search_list=[]
     
@@ -19240,7 +19395,7 @@ def multilex_scraper(input_dir, output_dir):
 
 
 
-    df_final_1 = [df170, df1, df2, df3, df4, df5, df6, df7, df11, df12, df13, df14, df15, df16, df17, df18, df19, df20 , df21, df22, df23, df24, df25, df26, df27, df28, df29, df30, df31, df32, df33, df34, df35, df36, df37 , df43,  df46, df49, df52,  df55, df57,  df60,  df63,  df66,  df69,  df72,  df75,  df78,  df81, df140,df146, df152,  df155,  df158,  df161,  df164,  df167,  df173,  df176,  df179,  df182,  df185,  df40,  df188,  df191,  df194,  df197,  df200,  df203, df207,  df210,  df213,  df216,  df219, df222,df225,df228,df231,df234,df237,df240,df243,df246,df249,df252,df255,df257,df258,df260,df261,df263,df264,df257,df258,df260,df261,df263,df264,df267,df270,df273,df276,df277,df280,df281,df284,df287,df288,df289,df292,df293,df294,df295,df296,df297,df298,df299,df300,df301,df302]
+    df_final_1 = [df170, df1, df2, df3, df4, df5, df6, df7, df11, df12, df13, df14, df15, df16, df17, df18, df19, df20 , df21, df22, df23, df24, df25, df26, df27, df28, df29, df30, df31, df32, df33, df34, df35, df36, df37 , df43,  df46, df49, df52,  df55, df57,  df60,  df63,  df66,  df69,  df72,  df75,  df78,  df81, df140,df146, df152,  df155,  df158,  df161,  df164,  df167,  df173,  df176,  df179,  df182,  df185,  df40,  df188,  df191,  df194,  df197,  df200,  df203, df207,  df210,  df213,  df216,  df219, df222,df225,df228,df231,df234,df237,df240,df243,df246,df249,df252,df255,df257,df258,df260,df261,df263,df264,df257,df258,df260,df261,df263,df264,df267,df270,df273,df276,df277,df280,df281,df284,df287,df288,df289,df292,df293,df294,df295,df296,df297,df298,df299,df300,df301,df302, df303]
     
     
        
--- DailyRunWF/Pipeline/Scraper/test_script.py
+++ DailyRunWF/Pipeline/Scraper/test_script.py
@@ -247,72 +247,85 @@ def multilex_scraper(input_dir, output_dir):
     
     def FilterFunction(final):
         
-        try:         
+        try:            
             if (final.empty):
                 return final
-            
-            key_1_gram = ['ipo', 'ipo', 'ipo ', 'spacs', 'ipo', 'pre-ipo', 'pre-ipo', 'pre-ipo', 'pre-ipo', 'spac', 'shares', 'pre ipo']
-            key_2_gram = ['ipo calendar', 'shelves ipo', 'halts ipo', 'ipo pipeline', 'withdraws ipo', 'eyes ipo', 'ipo-bound', 'ipo registration', 'red herring', 'pre-initial public', 'pre-ipo announcement', 'ipos scheduled', 'ipo scheduled', 'offering ipo', 'listed on', 'go public', 'plan to', 'going public', 'offering shares', 'initial public', 'public offering', 'have listed', 'files for']
-            
-            key_3_gram = ['begins ipo process', 'set to ipo', 'ipo open for', 'open for subscription', 'prices ipo', 'expected ipo filings', 'files for ipo', 'upcoming ipo', 'an initial public', 'offer its shares', 'to the public', 'going to list', 'files for ipo', 'filed for ipo', 'initial public offering', 'public offering ipo']
-            
-            key_4_gram = ['gets nod for ipo', 'fixed a price band', 'sets ipo price band', 'planning to go public', 'preparing to go public', 'ipo to be launched', 'an initial public offering', 'the initial public offering', 'its initial public offering', 'initial public offering ipo', 'the initial public offering', 'its initial public offering ', 'has set its ipo', 'targeting a 2023 ipo']
-            
-            key_5_gram = ['planning an initial public offering', 'files a prospectus for ipo', 'considering an initial public offering', 'for an initial public offering']
-            
+            final["title"] = final["title"].str.replace(' b ',' ')
+            final["text"] = final["text"].str.replace(' b ',' ')
+            final["title"] = final["title"].str.replace(' B ',' ')
+            final["text"] = final["text"].str.replace(' B ',' ')
+            key_1_gram = ['IPO', 'IPO', 'IPO ', 'SPACs', 'ipo', 'pre-IPO',
+                          'pre-ipo', 'PRE-IPO', 'pre-IPO', 'spac', 'shares', 'pre ipo']
+            key_2_gram = ['IPO Calendar','Shelves IPO','Halts IPO','IPO Pipeline','Withdraws IPO','Eyes IPO','IPO-Bound','IPO Registration','Red Herring','Pre-Initial Public','Pre-IPO announcement','IPOs scheduled','IPO scheduled','offering ipo', "listed on", "go public", "plan to", "going public",
+                          "offering shares", "initial public", "public offering", "have listed", "files for"]
+            key_3_gram = ['Begins IPO Process','Set to IPO','IPO open for','Open for subscription','Prices IPO','Expected IPO Filings','Files for IPO','Upcoming IPO',"an initial public", "offer its shares", "to the public", "going to list",
+                          "files for ipo", "filed for ipo", "initial public offering", "public offering ipo"]
+            key_4_gram = ['Gets nod for IPO','Fixed a price band','Sets IPO Price Band','Planning to go public','Preparing to go public','IPO to be launched',"an initial public offering", "the initial public offering", "its initial public offering","initial public offering ipo", 
+                          "The Initial Public Offering", "Its Initial Public Offering ", "Has Set Its Ipo", "Targeting A 2023 Ipo"]
+            key_5_gram = ["Planning An Initial Public Offering", "Files A Prospectus For Ipo", "Considering An Initial Public Offering",'for an initial public offering']
+
             key_6_gram = [ 'sebagai ungkapan terimakasih atas perhatian anda', 'ungkapan terimakasih atas perhatian anda tersedia', 'terimakasih atas perhatian anda tersedia voucer', 'atas perhatian anda tersedia voucer gratis', 'perhatian anda tersedia voucer gratis senilai', 'anda tersedia voucer gratis senilai donasi',
                           'tersedia voucer gratis senilai donasi yang', 'voucer gratis senilai donasi yang bisa', 'gratis senilai donasi yang bisa digunakan', 'senilai donasi yang bisa digunakan berbelanja', 'donasi yang bisa digunakan berbelanja di', 'b initial public offering b b', 'b initial public offering b of', 'initial public offering b b ipo', 'public offering b b ipo b', 'the b initial public offering b', "Will Hold An Initial Public Offering", "On Its Potential Initial Public Offering"]
-            key_7_gram = ["has filed for an initial public offering",'sebagai ungkapan terimakasih atas perhatian anda tersedia', 'ungkapan terimakasih atas perhatian anda tersedia voucer', 'terimakasih atas perhatian anda tersedia voucer gratis', 'atas perhatian anda tersedia voucer gratis senilai', 'perhatian anda tersedia voucer gratis senilai donasi', 'anda tersedia voucer gratis senilai donasi yang', 'tersedia voucer gratis senilai donasi yang bisa', 'voucer gratis senilai donasi yang bisa digunakan', 'gratis senilai donasi yang bisa digunakan berbelanja', 'senilai donasi yang bisa digunakan berbelanja di'
+            key_7_gram = ["Has Filed For An Initial Public Offering",'sebagai ungkapan terimakasih atas perhatian anda tersedia', 'ungkapan terimakasih atas perhatian anda tersedia voucer', 'terimakasih atas perhatian anda tersedia voucer gratis', 'atas perhatian anda tersedia voucer gratis senilai', 'perhatian anda tersedia voucer gratis senilai donasi', 'anda tersedia voucer gratis senilai donasi yang', 'tersedia voucer gratis senilai donasi yang bisa', 'voucer gratis senilai donasi yang bisa digunakan', 'gratis senilai donasi yang bisa digunakan berbelanja', 'senilai donasi yang bisa digunakan berbelanja di'
                           , 'dapat voucer gratis sebagai ungkapan terimakasih atas', 'voucer gratis sebagai ungkapan terimakasih atas perhatian', 'gratis sebagai ungkapan terimakasih atas perhatian anda', 'donasi yang bisa digunakan berbelanja di dukungan', 'yang bisa digunakan berbelanja di dukungan anda', 'bisa digunakan berbelanja di dukungan anda akan', "Raise Funds Through An Initial Public Offering"]
-            key_8_gram = ["listing",'initial public offering','initial public offfering','to go public','goes Public','goes public','restricted shares','traded','list','listed','listing','lifted','allotment','Nyse','will be listed','stock exchange','delisting','moved to','hit','funding','raise','raises','valuation','going public','subscription','spac','sells','stake','debut','fundraising','board','enrollment','trade','hits','revenues','expansion','rebrand','subscribe','purchase','target price','shares','investing','allotmnet','approved','approves','fpo','registration','funds','sebi','investment','offering','nasdaq','files','launch','fund', 'stock', 'stocks', 'aims to', 'explores options', 'spinoff', 'digest', 'securities', "offer price"]
+            key_8_gram = []
             title, link, published_date, scraped_date, text = [], [], [], [], []
-            final["accepted"]=""
             for i, row in final.iterrows():
-                
-                article = str(row["title"]).lower()
+                cases = [0]*8
+                article = str(str(row["title"]) + " " +
+                              str(row["text"])).lower()
                 # print(article + "\n\n\n\n")
-                c=0
-                for j in key_1_gram:
-                    if j in article:
-                        c+=1
-                        break
-                for j in key_2_gram:
-                    if j in article:
-                        c+=1
-                        break
-                
-                for j in key_3_gram:
-                    if j in article:
-                        c+=1
-                        break
-                
-                for j in key_4_gram:
-                    if j in article:
-                        c+=1
-                        break
-                
-                for j in key_5_gram:
-                    if j in article:
-                        c+=1
-                        break
-                
-                for j in key_8_gram:
-                    if j in article:
-                        c+=1
-                        break
-                        
-                if(c>0):
-                    final.at[i, "accepted"]="Yes"
-                else:
-                    final.at[i, "accepted"]="No"
-                c=0
-            final=final[final["accepted"]=="Yes"]
-            del final["accepted"]
-            final = final.reset_index(drop=True)
+                text_list = [article]
+                key_1_gram = conver_to_lower(key_1_gram)
+                key_2_gram = conver_to_lower(key_2_gram)
+                key_3_gram = conver_to_lower(key_3_gram)
+                key_4_gram = conver_to_lower(key_3_gram)
+                key_5_gram = conver_to_lower(key_3_gram)
+                key_6_gram = conver_to_lower(key_3_gram)
+                key_7_gram = conver_to_lower(key_3_gram)
+                # key_8_gram = conver_to_lower(key_3_gram)
+
+                res_1_gram = tokenize_no_words(text_list, 1)
+                res_2_gram = tokenize_no_words(text_list, 2)
+                res_3_gram = tokenize_no_words(text_list, 3)
+                res_4_gram = tokenize_no_words(text_list, 4)
+                res_5_gram = tokenize_no_words(text_list, 5)
+                res_6_gram = tokenize_no_words(text_list, 6)
+                res_7_gram = tokenize_no_words(text_list, 7)
+                # res_8_gram = tokenize_no_words(text_list, 8)
+                if (len(res_1_gram.intersection(key_1_gram)) > 0):
+                    cases[0] = 1
+                if (len(res_2_gram.intersection(key_2_gram)) > 0):
+                    cases[1] = 1
+                if (len(res_3_gram.intersection(key_3_gram))):
+                    cases[2] = 1
+                if (len(res_4_gram.intersection(key_4_gram))):
+                    cases[3] = 1
+                if (len(res_5_gram.intersection(key_5_gram))):
+                    cases[4] = 1
+                if (len(res_6_gram.intersection(key_6_gram))):
+                    cases[5] = 1
+                if (len(res_7_gram.intersection(key_7_gram))):
+                    cases[6] = 1
+                # if (len(res_8_gram.intersection(key_8_gram))):
+                #     cases[7] = 1
+                if (cases[0] and (cases[1] or cases[2] or cases[3] or cases[4] or cases[5] or cases[6] )):
+                    title.append(final['title'][i])
+                    link.append(final['link'][i])
+                    published_date.append(final['publish_date'][i])
+                    scraped_date.append(final['scraped_date'][i])
+                    text.append(final['text'][i])
+                cases = [0]*8
+            final = pd.DataFrame(list(zip(title, link, published_date, scraped_date, text)),
+                                 columns=['title', 'link', 'publish_date', 'scraped_date', 'text'])
+            final = final[~final['title'].isin(
+                ["private placement", "reverse merger", "blank check merger"])]
+            final = final[~final['text'].isin(
+                ["private placement", "reverse merger", "blank check merger"])]
+            # final.to_csv("Newshab.csv")
             return final
         except:
             print("Issue in Filter Function")
-
 
     def log_errors(err_logs):
         for i in err_logs:
@@ -363,7 +376,7 @@ def multilex_scraper(input_dir, output_dir):
     
     
     
-    df_final_1 = ["some_function()"] #call the function here
+    df_final_1 = ["somefunction()"] #call the function here
     
     
     
